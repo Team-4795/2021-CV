b7af2565f4486fd39828c7cca48f1dcd2f2e908f
Jake Boggs <jake.boggs.code@gmail.com>
2021-04-27 16:03:57 -0400

    Update 360noscope.py

--- 360noscope.py
+++ 360noscope.py
@@ -4,6 +4,8 @@ from networktables import NetworkTables
 
 green_lower = (60, 20, 230)
 green_upper = (95, 255, 255)
+horizontal_degrees_per_pixel = 0.2
+vertical_degrees_per_pixel = 0.3
 
 cap = cv2.VideoCapture(0)
 
@@ -23,6 +25,7 @@ while True:
 
 	mask = cv2.inRange(hsv, green_lower, green_upper)
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, (5, 5))
+	mask = cv2.dilate(mask, np.ones((5, 5), np.uint8), iterations=1)
 
 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 	options = []
@@ -30,19 +33,23 @@ while True:
 	for cnt in cnts:
 		x, y, w, h = cv2.boundingRect(cnt)
 
-		if w > 10 and h > 2 and w > h * 2 and h * 8 > w:
+		if w > 10 and h > 2 and w > h * 1.5 and h * 4 > w:
 			options.append(cnt)
 
 	if len(options) > 0:
 		cnt = max(options, key=cv2.contourArea)
 
+		M = cv2.moments(cnt)
+		cX = int(M['m10'] / M['m00'])
+		cY = int(M['m01'] / M['m00'])
+
+		distance = 84 / math.tan((cY - height / 2) * vertical_degrees_per_pixel)
+		angle = (cX - width / 2) * horizontal_degrees_per_pixel
+
 		x, y, w, h = cv2.boundingRect(cnt)
 
-		distance = 1.85 ** math.log(h / height, 0.6) / 12
-		angle = ((x + x + w) / 2 - width / 2) / width * 2 / distance * 175
-
-		# cv2.rectangle(frame, (x, y, w, h), (0, 0, 255), 3)
-		# cv2.putText(frame, str(round(distance, 2)) + ' feet, ' + str(round(angle, 2)) + ' degrees', (x, y), 0, 1.25e-3 * width, (0, 255, 255), 2)
+		cv2.rectangle(frame, (x, y, w, h), (0, 0, 255), 3)
+		cv2.putText(frame, str(round(distance, 2)) + ' inches, ' + str(round(angle, 2)) + ' degrees', (x, y), 0, 1.25e-3 * width, (0, 255, 255), 2)
 		
 		sd.putNumber('goal_distance', distance)
 		sd.putNumber('goal_angle', angle)
@@ -53,6 +60,6 @@ while True:
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
-# 	cv2.imshow('Frame', frame)
+	cv2.imshow('Frame', frame)
 
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
